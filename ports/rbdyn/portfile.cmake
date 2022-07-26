1ba51b8058009dfa55d9e6e7a88f786302a629a7
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-07-26 11:08:47 +0200

    [rbdyn] Update to 1.6.0

--- ports/rbdyn/portfile.cmake
+++ ports/rbdyn/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.5.2/RBDyn-v1.5.2.tar.gz"
-    FILENAME "RBDyn-v1.5.2.tar.gz"
-    SHA512 8683798f9569ebce9541b145c9d04f8b8b7f76b78099c146c498ae2959cd0066382ff503e21dd227205f35a4a951dd909b781163f972d9de7a704dad293bfdb1
+    URLS "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.6.0/RBDyn-v1.6.0.tar.gz"
+    FILENAME "RBDyn-v1.6.0.tar.gz"
+    SHA512 fc3d7761cd0f15386326512b6787b6b85a32e8c31b1b1c99c3c7f1be63a6e494e61ad889c252cc97137ad6016f03fa4b355da1f580d6fb6826fc062a9467419a
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 1.5.2
+    REF 1.6.0
 )
 
 vcpkg_configure_cmake(
