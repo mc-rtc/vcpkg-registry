e232406bea2faad3114a5cce3d78eec9a046c8ac
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-05-22 11:04:35 +0900

    [tasks] Update to 1.7.2

--- ports/tasks/portfile.cmake
+++ ports/tasks/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/Tasks/releases/download/v1.7.0/Tasks-v1.7.0.tar.gz"
-    FILENAME "Tasks-v1.7.0.tar.gz"
-    SHA512 f18cdc4c28af65e2bb4d4fed80e8306d92a2ac91c804afddb03fa4eb18db996019e5ff06f9b41210048dcdafd6fa7ef7c5af7b1097e7258c44b0c9b6c73d5de6
+    URLS "https://github.com/jrl-umi3218/Tasks/releases/download/v1.7.2/Tasks-v1.7.2.tar.gz"
+    FILENAME "Tasks-v1.7.2.tar.gz"
+    SHA512 e180eb5ab6d272f2c9b3525f6fe657d2e4dc3ab73a0e8fa0a434af49adf35e0a97f766d13a2545529e67fdfb196c8702f19bbcef79d8ba01ed9402a4dd39c298
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 1.7.0
+    REF 1.7.2
 )
 
 vcpkg_configure_cmake(
