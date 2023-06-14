cf47678fcbb5298285ebb8ffa47f3ac5bbd106a6
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-06-14 09:44:18 +0900

    [spacevecalg] Update to 1.2.5

--- ports/spacevecalg/portfile.cmake
+++ ports/spacevecalg/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/SpaceVecAlg/releases/download/v1.2.4/SpaceVecAlg-v1.2.4.tar.gz"
-    FILENAME "SpaceVecAlg-v1.2.4.tar.gz"
-    SHA512 5cd16be6f69f3e271792cb802a12b48ce59bea7708fc861db751e4b21c6131a3d6185732208efb8e6e9a3f6c2e69f9f40c6684b1fb5b96c276f321a4edadf0d5
+    URLS "https://github.com/jrl-umi3218/SpaceVecAlg/releases/download/v1.2.5/SpaceVecAlg-v1.2.5.tar.gz"
+    FILENAME "SpaceVecAlg-v1.2.5.tar.gz"
+    SHA512 4303d54693801f5d9e136c1320b45b1929489f55d7affed39831ac2b8b0f7c022bdeffbf87e40f0fc6164389a187d0aae0636d5f97bcef70b9d47aae618ede65
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 1.2.4
+    REF 1.2.5
 )
 
 vcpkg_configure_cmake(
