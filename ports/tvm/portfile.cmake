a9ad6edc4ed8352006fbc322b8b0b1a398f20cee
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-06-14 09:44:27 +0900

    [tvm] Update to 0.9.1

--- ports/tvm/portfile.cmake
+++ ports/tvm/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/tvm/releases/download/v0.9.0/tvm-v0.9.0.tar.gz"
-    FILENAME "tvm-v0.9.0.tar.gz"
-    SHA512 6f987bd6df7d9b69f259eaa0ac247612e21bf11c7c3174e5630bd2fdd8b5cefdd2894c245fb2cc391c51c01af8e23cf1b3649a53c18af2227162943b3f015e3a
+    URLS "https://github.com/jrl-umi3218/tvm/releases/download/v0.9.1/tvm-v0.9.1.tar.gz"
+    FILENAME "tvm-v0.9.1.tar.gz"
+    SHA512 d9b2e50fd4e1c491db3fb26e273ce0e74c5c247afdf4611d5adcfc8ed082d2480c8e6ad6776cb53a5989c618d735431ada3370f4a29ae722107daee47938b16a
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 0.9.0
+    REF 0.9.1
 )
 
 vcpkg_configure_cmake(
