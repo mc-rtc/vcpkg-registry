902551abafbf6327adebab0dcef037d3b2251b53
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-06-14 09:44:20 +0900

    [rbdyn] Update to 1.8.2

--- ports/rbdyn/portfile.cmake
+++ ports/rbdyn/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.8.1/RBDyn-v1.8.1.tar.gz"
-    FILENAME "RBDyn-v1.8.1.tar.gz"
-    SHA512 c82119d044484ca8e462993e34ba877806fe32358bbe6efe0e072b7b746389490cfe32212346c6e691de436dca71dadb57161a5eeca060cfaf982bcda875fad0
+    URLS "https://github.com/jrl-umi3218/RBDyn/releases/download/v1.8.2/RBDyn-v1.8.2.tar.gz"
+    FILENAME "RBDyn-v1.8.2.tar.gz"
+    SHA512 30176f4f0f9c1f49db396291c0a9ade9a7aeb3617ddc295ef7f9d8415f0197c60d3d30d565b1c220a54294a53daf3cbfc4f7d90f1908803f87461a7321dc0e8d
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 1.8.1
+    REF 1.8.2
 )
 
 vcpkg_configure_cmake(
