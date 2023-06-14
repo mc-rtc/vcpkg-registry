745f2560011b7f4a09968e1060600e678a417e31
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-06-14 09:44:22 +0900

    [sch-core] Update to 1.4.2

--- ports/sch-core/portfile.cmake
+++ ports/sch-core/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/sch-core/releases/download/v1.4.1/sch-core-v1.4.1.tar.gz"
-    FILENAME "sch-core-v1.4.1.tar.gz"
-    SHA512 a8da81692066c0c990f590db324ee2763f5cda65469791247a7772385cf007ec2ca61c6d910feb0bbbc07f49bbe4156495e41f5764c4c896210f7c5060e98a50
+    URLS "https://github.com/jrl-umi3218/sch-core/releases/download/v1.4.2/sch-core-v1.4.2.tar.gz"
+    FILENAME "sch-core-v1.4.2.tar.gz"
+    SHA512 6d9580ec2868dc264571978b5b98ebc8a91a9b9bdccb9806ee57895521069ba594a23179a57361b60ad8a4dc87cd7a45f52e6b3a6ef20c22e1fb8569e5627fd7
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 1.4.1
+    REF 1.4.2
 )
 
 vcpkg_configure_cmake(
