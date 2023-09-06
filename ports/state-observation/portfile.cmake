7d611a4147fea8629520ddc44f07dbb612246b4e
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-09-06 12:39:43 +0900

    [state-observation] Update to 1.5.2

--- ports/state-observation/portfile.cmake
+++ ports/state-observation/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jrl-umi3218/state-observation/releases/download/v1.5.1/state-observation-v1.5.1.tar.gz"
-    FILENAME "state-observation-v1.5.1.tar.gz"
-    SHA512 31c2af261222714ff264804d86a1272c7f4436100d402f8b19ae19256bead5cb8b2f0f5cd160c4c20a24e499c863accb53d54f3d73a1b36e7e358818420d1b5c
+    URLS "https://github.com/jrl-umi3218/state-observation/releases/download/v1.5.2/state-observation-v1.5.2.tar.gz"
+    FILENAME "state-observation-v1.5.2.tar.gz"
+    SHA512 c3351a838d1ffe1569238a769c2c3c0c43e27fd56bb50d114e0d03ec2f4da7b46e325fc7c9f6c191136dfba8a0d6ed78014e341fd166cf6f3a8832b11271e0c7
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    REF 1.5.1
+    REF 1.5.2
 )
 
 vcpkg_configure_cmake(
